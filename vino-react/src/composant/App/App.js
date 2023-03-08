7f86384504b30770580d76d09d483c87e1ea3412
TommyFavreau <tommyfavreau@gmail.com>
2023-03-08 12:00:34 -0500

    Réorganisation du dossier src pour y ajouter un dossier composant et img

---
+++ vino-react/src/composant/App/App.js
@@ -0,0 +1,15 @@
+import logo from './foto.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>VINO</h1>
+        <img src={logo} alt="" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
