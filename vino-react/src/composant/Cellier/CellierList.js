647d8689ae259d5740693bfc5d209a9159ab6976
Eric Charbonnier <eric2503@hotmail.com>
2023-03-09 13:30:42 -0500

    création du fetch/affichage du data de CellierList

--- vino-react/src/composant/Cellier/CellierList.js
+++ vino-react/src/composant/Cellier/CellierList.js
@@ -1,14 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Cellier.css';
 
 function CellierList() {
-    return (
+  const [celliers, setCelliers] = useState([]);
 
-            <div className="accueilLink">
-                <Link to="/" className="btnAction">Retour à l'accueil</Link>
-            </div>
-    
-    );
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/cellier')
+      .then(reponse => reponse.json())
+      .then(data => {
+        setCelliers(data);
+        console.log(data);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Celliers</h1>
+      {celliers.length > 0 ? (
+        <ul>
+          {celliers.map(cellier => (
+            <li key={cellier.id}>
+              <Link to={"/cellier/" + cellier.id}>{cellier.nom}</Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Aucun cellier trouvé</p>
+      )}
+      <Link to="/cellier/create">Créer un nouveau cellier</Link>
+    </div>
+  );
 }
 
 export default CellierList;
