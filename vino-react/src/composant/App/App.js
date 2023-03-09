a9dc13562e1aa247931119a1b0e1d2fa36f80b50
Eric Charbonnier <eric2503@hotmail.com>
2023-03-09 18:29:17 -0500

    ajustement des imports

--- vino-react/src/composant/App/App.js
+++ vino-react/src/composant/App/App.js
@@ -5,8 +5,9 @@ import Footer from '../Footer/Footer';
 import Bouteille from '../Bouteille/Bouteille';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Accueil from '../Accueil/Accueil';
-// import CellierList from './components/Cellier/CellierList';
 import CellierList from '../Cellier/CellierList';
+import CellierCreate from '../Cellier/CellierCreate';
+import CellierShow from '../Cellier/CellierShow';
 
 export default class App extends React.Component {
 
@@ -21,6 +22,8 @@ export default class App extends React.Component {
         <Routes className="AppBody">
           <Route path="/" element={<Accueil />} />
           <Route path="/cellier" element={<CellierList />} />
+          <Route path="/cellier/:id" element={<CellierShow />} />
+          <Route path="/cellier/create" element={<CellierCreate />} />
           <Route path="/bouteille/:id" element={<Bouteille />} />
         </Routes>
         <Footer />
