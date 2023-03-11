8ae75ee6145a839527fd92e52829d8b11785c7be
TommyFavreau <tommyfavreau@gmail.com>
2023-03-11 12:57:53 -0500

    Design de la vue CellierList avec les liens

--- vino-react/src/composant/Cellier/CellierList.js
+++ vino-react/src/composant/Cellier/CellierList.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Cellier.css';
+import iconeEdit from '../../img/edit-icone-blanc.svg';
+import iconeAjout from '../../img/icone-ajout.svg';
 
 function CellierList() {
   const [celliers, setCelliers] = useState([]);
@@ -16,20 +18,31 @@ function CellierList() {
   }, []);
 
   return (
-    <div>
-      <h1>Celliers</h1>
+    <div class="container">
+      <div class="cellier__titre">
+        <h1>Mes celliers</h1>
+      </div>
       {celliers.length > 0 ? (
-        <ul>
+        <ul class="cellier">
           {celliers.map(cellier => (
-            <li key={cellier.id}>
-              <Link to={"/cellier/" + cellier.id}>{cellier.nom}</Link>
-            </li>
+            <Link to={"/cellier/" + cellier.id}>
+              <li class="cellier__carte" key={cellier.id}>
+                <div class="cellier__infos">
+                  <p class="cellier__infos--nom">{cellier.nom}</p>
+                  <img class="cellier__infos--edit" src={iconeEdit} alt="Editer" />
+                </div>
+              </li>
+            </Link>
           ))}
         </ul>
       ) : (
         <p>Aucun cellier trouvé</p>
       )}
-      <Link to="/cellier/create">Créer un nouveau cellier</Link>
+      <div class="cellier__ajouter">
+        <Link to="/cellier/create">
+          <img src={iconeAjout} alt="Ajouter" />
+        </Link>
+      </div>
     </div>
   );
 }
