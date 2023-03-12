beba6e95aff5f0d2f871dc4cd1a6c999d8e55395
Eric Charbonnier <eric2503@hotmail.com>
2023-03-11 22:48:46 -0500

    ajout du lien direct vers les celliers sur accueil

--- vino-react/src/composant/Accueil/Accueil.js
+++ vino-react/src/composant/Accueil/Accueil.js
@@ -4,30 +4,33 @@ import { Link } from 'react-router-dom';
 import logoVino from '../../img/vinoLogo-rouge.svg';
 
 export default class Accueil extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        return (
-            <div className="pageAccueil">
-                <div className="logo">
-                    <img src={logoVino} ></img>
-                </div>
-                <div className="titreAccueil">
-                    <h1>
-                        Votre cellier à portée de main
-                    </h1>
-                </div>
-                <div className="boutonsDiv">
-                    <div>
-                        <Link to="/Connexion" ><button className="btn btnConnexion">Se connecter</button></Link>
-                    </div>
-                    <div>
-                        <Link to="/Inscription" ><button className="btn btnInscription">S'inscrire</button></Link>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="pageAccueil">
+        <div className="logo">
+          <img src={logoVino} ></img>
+        </div>
+        <div className="titreAccueil">
+          <h1>
+            Votre cellier à portée de main
+          </h1>
+        </div>
+        <div className="boutonsDiv">
+          <div>
+            <Link to="/Connexion" ><button className="btn btnConnexion">Se connecter</button></Link>
+          </div>
+          <div>
+            <Link to="/Inscription" ><button className="btn btnInscription">S'inscrire</button></Link>
+          </div>
+          <div>
+            <Link to="/Cellier" ><button className="btn btnConnexion">Vers la page cellier</button></Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
