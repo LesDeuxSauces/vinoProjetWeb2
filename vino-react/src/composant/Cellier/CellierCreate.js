e35c9999d5071bd1e2e6ffffe30b188863823240
Rosember10 <rosember.01@outlook.com>
2023-03-14 15:03:30 -0400

    ajout de style css pour les inputs du formulaire ajouter cellier,inscription, connexion

--- vino-react/src/composant/Cellier/CellierCreate.js
+++ vino-react/src/composant/Cellier/CellierCreate.js
@@ -1,46 +1,68 @@
-import './Cellier.css';
+import "./Cellier.css";
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function CellierCreate() {
-  const [nom, setNom] = useState(''); // on crée un état pour le nom du cellier
+    const [nom, setNom] = useState(""); // on crée un état pour le nom du cellier
 
-  function handleSubmit(evt) { // gestion de l'envoi du formulaire
-    evt.preventDefault(); // empêche le rechargement de la page
-    const data = { nom }; // on crée un objet avec les données du formulaire
+    function handleSubmit(evt) {
+        // gestion de l'envoi du formulaire
+        evt.preventDefault(); // empêche le rechargement de la page
+        const data = { nom }; // on crée un objet avec les données du formulaire
 
-    fetch('http://127.0.0.1:8000/api/cellier', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    })
-      .then(reponse => reponse.json())
-      .then(data => {
-        console.log(data);
-        // Rediriger l'utilisateur vers la page de liste des celliers
-        window.location.pathname = '/Cellier'; // on redirige l'utilisateur vers la page de liste des celliers
-      })
-      .catch(error => console.error(error));
-  }
+        fetch("http://127.0.0.1:8000/api/cellier", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+        })
+            .then((reponse) => reponse.json())
+            .then((data) => {
+                console.log(data);
+                // Rediriger l'utilisateur vers la page de liste des celliers
+                window.location.pathname = "/Cellier"; // on redirige l'utilisateur vers la page de liste des celliers
+            })
+            .catch((error) => console.error(error));
+    }
 
-  function handleNomChange(evt) { // gestion de la modification du nom du cellier
-    setNom(evt.target.value); // on met à jour l'état du nom du cellier
-  }
+    function handleNomChange(evt) {
+        // gestion de la modification du nom du cellier
+        setNom(evt.target.value); // on met à jour l'état du nom du cellier
+    }
 
-  return (
-    <div>
-      <div className="cellier__titre">Créer un nouveau Cellier</div>
-      <form className="cellier__form" onSubmit={handleSubmit}>
+    return (
         <div>
-          <label>
-            <input className="cellier__input--nom" type="text" id="nom" name="nom" value={nom} onChange={handleNomChange} placeholder="Nom" required />
-          </label>
+            <div className="cellier__titre">Créer un nouveau Cellier</div>
+            <form className="cellier__form" onSubmit={handleSubmit}>
+            
+                <div className="form__group field input__ajouterCellier ">
+                    <input
+                        placeholder="Nom"
+                        className="form__field "
+                        id="nom"
+                        name="nom"
+                        type="text"
+                        value={nom}
+                        onChange={handleNomChange}
+                        
+                    />
+                    <label className="form__label">Nom</label>
+                </div>
+             
+                <div className="cellier__btn--div">
+                    <button
+                        className="cellier__btn--style cellier__btn--creer"
+                        type="submit"
+                    >
+                        Créer votre cellier
+                    </button>
+                    <Link
+                        to="/Cellier"
+                        className="cellier__btn--style cellier__btn--retour"
+                    >
+                        Retour à la liste des celliers
+                    </Link>
+                </div>
+            </form>
         </div>
-        <div className="cellier__btn--div">
-          <button className="cellier__btn--style cellier__btn--creer" type="submit">Créer votre cellier</button>
-          <Link to="/Cellier" className="cellier__btn--style cellier__btn--retour">Retour à la liste des celliers</Link>
-        </div>
-      </form>
-    </div>
-  );
+    );
 }
--- vino-react/src/composant/Connexion/Connexion.js
+++ vino-react/src/composant/Connexion/Connexion.js
@@ -1,90 +1,116 @@
-import './Connexion.css';
-import { Route, Routes, BrowserRouter, useNavigate, Link } from 'react-router-dom';
+import "./Connexion.css";
+import {
+    Route,
+    Routes,
+    BrowserRouter,
+    useNavigate,
+    Link,
+} from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import logoVino from '../../img/vinoLogo-rouge.svg';
-
-
+import logoVino from "../../img/vinoLogo-rouge.svg";
 
 export default function Connexion() {
+    const api_url = "http://127.0.0.1:8000/api/";
+    const navigate = useNavigate();
 
-  const api_url = "http://127.0.0.1:8000/api/";
-  const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    useEffect(() => {
+        //
+    }, []);
 
-  useEffect(() => {
-    //
-  }, []);
+    async function loginUser(userObject) {
+        let entete = new Headers();
+        entete.append("Content-Type", "application/json");
 
-  async function loginUser(userObject) {
+        const response = await fetch(api_url + "login", {
+            method: "POST",
+            body: JSON.stringify(userObject),
+            headers: entete,
+        });
 
-    let entete = new Headers();
-    entete.append("Content-Type", "application/json");
+        const responseCode = await response.json();
+        const accessToken = responseCode.access_token;
+        console.log(accessToken);
+        console.log("reponse connexion utilsateur:", responseCode);
+        navigate("/cellier", { state: { data: accessToken } });
+    }
 
-    const response = await fetch(api_url + "login", {
-      method: "POST",
-      body: JSON.stringify(userObject),
-      headers: entete,
-    });
+    const emailChangeHandler = (event) => {
+        // console.log(event.target.value);
+        setEmail(event.target.value);
+    };
 
-    const responseCode = await response.json();
-    const accessToken = responseCode.access_token;
-    console.log(accessToken);
-    console.log("reponse connexion utilsateur:", responseCode);
-    navigate('/cellier', { state: { data: accessToken } });
+    const passwordChangeHandler = (event) => {
+        // console.log(event.target.value);
+        setPassword(event.target.value);
+    };
 
-  }
+    const submitHandler = (event) => {
+        event.preventDefault();
+        loginUser({ email: email, password: password });
+    };
 
-
-  const emailChangeHandler = (event) => {
-    // console.log(event.target.value);
-    setEmail(event.target.value);
-  };
-
-  const passwordChangeHandler = (event) => {
-    // console.log(event.target.value);
-    setPassword(event.target.value);
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    loginUser({ email: email, password: password });
-
-  };
-
-  return (
-
-    <section>
-      <div>
-        <div className="connexion__header">
-          <Link to="/">
-            <img className="logo__connection--hover" src={logoVino} alt="Vino Logo" />
-          </Link>
-          <div className="connexion__titre">Connexion</div>
-        </div>
-        <form className="connexion__form" onSubmit={submitHandler}>
-          <div>
-            <label>
-              <img src="" alt="" />
-              <input className="connexion__input" type="text" value={email} onChange={emailChangeHandler} placeholder="Email" required />
-            </label>
-          </div>
-          <div>
-            <label>
-              <img src="" alt="" />
-              <input className="connexion__input" type="text" value={password} onChange={passwordChangeHandler} placeholder="Mot de passe" required />
-            </label>
-          </div>
-          <div className="connexion__bouton">
-            <button className="connexion__bouton--btn" type="submit">Se connecter</button>
-            <p>Vous n'avez pas de compte ? <Link to="/inscription" className="">Cliquez ici</Link> </p>
-          </div>
-        </form>
-      </div>
-    </section>
-  );
-
+    return (
+        <section>
+            <div>
+                <div className="connexion__header">
+                    <Link to="/">
+                        <img
+                            className="logo__connection--hover"
+                            src={logoVino}
+                            alt="Vino Logo"
+                        />
+                    </Link>
+                    <div className="connexion__titre">Connexion</div>
+                </div>
+                <form className="connexion__form" onSubmit={submitHandler}>
+                    
+                        <div className="form__group field">
+                            <input
+                                placeholder="Email"
+                                className="form__field"
+                                id="email"
+                                name="email"
+                                type="text"
+                                value={email}
+                                onChange={emailChangeHandler}
+                                required
+                            />
+                            <label className="form__label">Courriel</label>
+                        </div>
+                        <div className="form__group field">
+                            <input
+                                placeholder="Mot de passe"
+                                className="form__field"
+                                id="password"
+                                name="password"
+                                type="text"
+                                value={password}
+                                onChange={passwordChangeHandler}
+                                required
+                            />
+                            <label className="form__label">Mot de passe</label>
+                        </div>
+                  
+          
+                    <div className="connexion__bouton">
+                        <button
+                            className="connexion__bouton--btn"
+                            type="submit"
+                        >
+                            Se connecter
+                        </button>
+                        <p>
+                            Vous n'avez pas de compte ?{" "}
+                            <Link to="/inscription" className="">
+                                Cliquez ici
+                            </Link>{" "}
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
 }
-
-
