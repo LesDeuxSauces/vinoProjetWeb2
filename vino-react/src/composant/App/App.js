cb2d4ed313a90e0b02cbd30faa9bea17921d20cc
CorentinDelalle <corentindelalle@gmail.com>
2023-03-18 17:53:17 -0400

    Modif du router, créa PageErreur et Logout

--- vino-react/src/composant/App/App.js
+++ vino-react/src/composant/App/App.js
@@ -5,55 +5,75 @@ import BouteilleList from '../Bouteille/BouteilleList';
 import BouteilleShow from '../Bouteille/BouteilleShow';
 import BouteilleCreate from '../Bouteille/BouteilleCreate';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Accueil from '../Accueil/Accueil';
 import Inscription from '../Inscription/Inscription';
 import CellierList from '../Cellier/CellierList';
 import CellierCreate from '../Cellier/CellierCreate';
 import CellierShow from '../Cellier/CellierShow';
 import Connexion from '../Connexion/Connexion';
+import PageErreur from '../PageErreur/PageErreur';
 import { useLocation } from 'react-router-dom';
 
-function AppRoutes() {
-  const location = useLocation();
+const router = createBrowserRouter([
+  {path: '/', element: <Accueil />, errorElement: <PageErreur/>},
+  {path: '/inscription', element: <Inscription />},
+  {path: '/connexion', element: <Connexion />},
+  {path: '/', 
+   element: <Entete />,
+   children: [
+    {path:"/cellier", element:<CellierList /> },
+    {path:"/cellier/:id", element:<CellierShow />},
+    {path:"/cellier/create", element:<CellierCreate />},
+    {path:"/bouteille", element:<BouteilleList />},
+    {path:"/bouteille/create/:idCellier", element:<BouteilleCreate />},
+    {path:"/bouteille/:id", element:<BouteilleShow />},
+   ]
+  },
+])
 
-  if (location.pathname === '/' || location.pathname === '/connexion' || location.pathname === '/inscription' || location.pathname === '/Connexion' || location.pathname === '/Inscription') {
-    return (
-      <Routes className="AppBody">
-        <Route path="/" element={<Accueil />} />
-        <Route path="/inscription" element={<Inscription />} />
-        <Route path="/connexion" element={<Connexion />} />
-        <Route path="/cellier" element={<CellierList />} />
-        <Route path="/cellier/:id" element={<CellierShow />} />
-        <Route path="/cellier/create" element={<CellierCreate />} />
-        <Route path="/bouteille" element={<BouteilleList />} />
-        <Route path="/bouteille/create/:idCellier" element={<BouteilleCreate />} />
-        <Route path="/bouteille/:id" element={<BouteilleShow />} />
-      </Routes>
-    );
-  } else {
-    return (
-      <>
-        <Entete />
-        <Routes className="AppBody">
-          <Route path="/" element={<Accueil />} />
-          <Route path="/inscription" element={<Inscription />} />
-          <Route path="/connexion" element={<Connexion />} />
-          <Route path="/cellier" element={<CellierList />} />
-          <Route path="/cellier/:id" element={<CellierShow />} />
-          <Route path="/cellier/create" element={<CellierCreate />} />
-          <Route path="/bouteille" element={<BouteilleList />} />
-          <Route path="/bouteille/create/:idCellier" element={<BouteilleCreate />} />
-          <Route path="/bouteille/:id" element={<BouteilleShow />} />
-        </Routes>
-      </>
-    );
-  }
-}
+// function AppRoutes() {
+//   const location = useLocation();
+
+//   if (location.pathname === '/' || location.pathname === '/connexion' || location.pathname === '/inscription' || location.pathname === '/Connexion' || location.pathname === '/Inscription') {
+//     return (
+//       <Routes className="AppBody">
+//         <Route path="/" element={<Accueil />} />
+//         <Route path="/inscription" element={<Inscription />} />
+//         <Route path="/connexion" element={<Connexion />} />
+//         <Route path="/cellier" element={<CellierList />} />
+//         <Route path="/cellier/:id" element={<CellierShow />} />
+//         <Route path="/cellier/create" element={<CellierCreate />} />
+//         <Route path="/bouteille" element={<BouteilleList />} />
+//         <Route path="/bouteille/create/:idCellier" element={<BouteilleCreate />} />
+//         <Route path="/bouteille/:id" element={<BouteilleShow />} />
+//       </Routes>
+//     );
+//   } else {
+//     return (
+//       <>
+//         <Entete />
+//         <Routes className="AppBody">
+//           <Route path="/" element={<Accueil />} />
+//           <Route path="/inscription" element={<Inscription />} />
+//           <Route path="/connexion" element={<Connexion />} />
+//           <Route path="/cellier" element={<CellierList />} />
+//           <Route path="/cellier/:id" element={<CellierShow />} />
+//           <Route path="/cellier/create" element={<CellierCreate />} />
+//           <Route path="/bouteille" element={<BouteilleList />} />
+//           <Route path="/bouteille/create/:idCellier" element={<BouteilleCreate />} />
+//           <Route path="/bouteille/:id" element={<BouteilleShow />} />
+//         </Routes>
+//       </>
+//     );
+//   }
+// }
 
 export default function App() {
-  return (
-    <Router id="App">
-      <AppRoutes />
-    </Router>
-  );
+  return <RouterProvider router={router} />
+  // return (
+  //   <Router id="App">
+  //     <AppRoutes />
+  //   </Router>
+  // );
 }
--- vino-react/src/composant/Entete/Entete.js
+++ vino-react/src/composant/Entete/Entete.js
@@ -1,138 +1,157 @@
-import './Entete.css';
-import logoVino from '../../img/vinoLogo-blanc.svg';
-import { Link } from 'react-router-dom';
-import { ReactComponent as CaretIcon } from '../../img/caret.svg';
-import { ReactComponent as CogIcon } from '../../img/cog.svg';
-import { ReactComponent as ChevronIcon } from '../../img/chevron.svg';
-import { ReactComponent as ChevronLeftIcon } from '../../img/chevronLeft.svg';
-import { ReactComponent as BoltIcon } from '../../img/bolt.svg';
-import { ReactComponent as BurgerIcon } from '../../img/menu_burger_blanc.svg';
+import "./Entete.css";
+import logoVino from "../../img/vinoLogo-blanc.svg";
+import { Link, json, Outlet } from "react-router-dom";
+import { ReactComponent as CaretIcon } from "../../img/caret.svg";
+import { ReactComponent as CogIcon } from "../../img/cog.svg";
+import { ReactComponent as ChevronIcon } from "../../img/chevron.svg";
+import { ReactComponent as ChevronLeftIcon } from "../../img/chevronLeft.svg";
+import { ReactComponent as BoltIcon } from "../../img/bolt.svg";
+import { ReactComponent as BurgerIcon } from "../../img/menu_burger_blanc.svg";
 // import { ReactComponent as BurgerIcon } from '../../img/menuBurgerRouge.svg';
-import React, { useState, useEffect, useRef } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import React, { useState, useEffect, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 
 function Entete() {
-    return (
-        <Navbar>
-            <Link to="/"> 
-            <img className="entete__logo" src={logoVino} alt="" onClick={"/"} />
-            </Link> 
+  return (
+    <>
+      <Navbar>
+        <Link to="/">
+          <img className="entete__logo" src={logoVino} alt="" onClick={"/"} />
+        </Link>
 
-            <NavItem icon={<BurgerIcon />}>
-                <DropdownMenu></DropdownMenu>
-            </NavItem>
-            
-        </Navbar>
-    );
+        <NavItem icon={<BurgerIcon />}>
+          <DropdownMenu></DropdownMenu>
+        </NavItem>
+      </Navbar>
+
+      <Outlet />
+    </>
+  );
 }
 
 function Navbar(props) {
-    return (
-        <nav className="navbar">
-            <ul className="navbar-nav">{props.children}</ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
 }
 
 function NavItem(props) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return (
-        <li className="nav-item">
-            <a href="#" className="icon-buttonMain" onClick={() => setOpen(!open)}>
-                {props.icon}
-            </a>
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-buttonMain" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
 
-            {open && props.children}
-        </li>
-    );
+      {open && props.children}
+    </li>
+  );
 }
 
 function DropdownMenu() {
-    const [activeMenu, setActiveMenu] = useState('main');
-    const [menuHeight, setMenuHeight] = useState(null);
-    const dropdownRef = useRef(null);
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
 
-    useEffect(() => {
-        setMenuHeight(dropdownRef.current?.firstChild.offsetHeight +30)
-    }, [])
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight + 30);
+  }, []);
 
-    function calcHeight(el) {
-        const height = el.offsetHeight + 30;
-        console.log(height);
-        setMenuHeight(height);
-    }
+  function calcHeight(el) {
+    const height = el.offsetHeight + 30;
+    console.log(height);
+    setMenuHeight(height);
+  }
 
-    function DropdownItem(props) {
-        return (
-            <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-                <span className="icon-button">{props.leftIcon}</span>
-                {props.children}
-                <span className="icon-right">{props.rightIcon}</span>
-            </a>
-        );
-    }
-
+  function DropdownItem(props) {
     return (
-        <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
-
-            <CSSTransition
-                in={activeMenu === 'main'}
-                timeout={500}
-                classNames="menu-primary"
-                unmountOnExit
-                onEnter={calcHeight}>
-                <div className="menu">
-                    <DropdownItem leftIcon={<ChevronIcon />}>Mon profil</DropdownItem>
-                    <DropdownItem
-                        leftIcon={<ChevronIcon />}
-                        rightIcon={<ChevronIcon />}
-                        goToMenu="celliers">
-                        Mes Celliers
-                    </DropdownItem>
-                    <DropdownItem
-                        leftIcon={<CogIcon />}
-                        rightIcon={<ChevronIcon />}
-                        goToMenu="params">
-                        Paramètres
-                    </DropdownItem>
-                    <DropdownItem leftIcon={<BoltIcon />}>Déconnexion</DropdownItem>
-                </div>
-            </CSSTransition>
-
-            <CSSTransition
-                in={activeMenu === 'params'}
-                timeout={500}
-                classNames="menu-secondary"
-                unmountOnExit
-                onEnter={calcHeight}>
-                <div className="menu">
-                    <DropdownItem goToMenu="main" leftIcon={<ChevronLeftIcon />}>
-                        <h2>Paramètres</h2>
-                    </DropdownItem>
-                    <DropdownItem leftIcon={<CogIcon />}>Thème</DropdownItem>
-                    <DropdownItem leftIcon={<CogIcon />}>Unitées</DropdownItem>
-                </div>
-            </CSSTransition>
-
-            <CSSTransition
-                in={activeMenu === 'celliers'}
-                timeout={500}
-                classNames="menu-secondary"
-                unmountOnExit
-                onEnter={calcHeight}>
-                <div className="menu">
-                    <DropdownItem leftIcon={<ChevronLeftIcon />} goToMenu="main" >
-                        <h2>Mes Celliers</h2>
-                    </DropdownItem>
-                    <DropdownItem leftIcon={<ChevronIcon />}>Maison</DropdownItem>
-                    <DropdownItem leftIcon={<ChevronIcon />}>Chalet</DropdownItem>
-                    <DropdownItem leftIcon={<ChevronIcon />}>Manoir en France</DropdownItem>
-                    <DropdownItem leftIcon={<ChevronIcon />}>La villa en españa</DropdownItem>
-                </div>
-            </CSSTransition>
-        </div>
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
     );
+  }
+
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem leftIcon={<ChevronIcon />}>Mon profil</DropdownItem>
+          <DropdownItem
+            leftIcon={<ChevronIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="celliers"
+          >
+            Mes Celliers
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="params"
+          >
+            Paramètres
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>
+            {" "}
+            Déconnexion
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "params"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ChevronLeftIcon />}>
+            <h2>Paramètres</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<CogIcon />}>Thème</DropdownItem>
+          <DropdownItem leftIcon={<CogIcon />}>Unitées</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "celliers"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem leftIcon={<ChevronLeftIcon />} goToMenu="main">
+            <h2>Mes Celliers</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<ChevronIcon />}>Maison</DropdownItem>
+          <DropdownItem leftIcon={<ChevronIcon />}>Chalet</DropdownItem>
+          <DropdownItem leftIcon={<ChevronIcon />}>
+            Manoir en France
+          </DropdownItem>
+          <DropdownItem leftIcon={<ChevronIcon />}>
+            La villa en españa
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  );
 }
 
 export default Entete;
