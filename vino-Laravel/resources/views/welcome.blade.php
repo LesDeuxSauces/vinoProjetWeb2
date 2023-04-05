46afd252da87fc8d669b4aa925b1eb19fe0c9586
TommyFavreau <tommyfavreau@gmail.com>
2023-04-05 12:05:45 -0400

    Suppression de code inutile pour la page d'accueil de l'API Vino

--- vino-Laravel/resources/views/welcome.blade.php
+++ vino-Laravel/resources/views/welcome.blade.php
@@ -4,21 +4,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>VINO</title>
+  <title>API VINO</title>
 </head>
 <body>
   
-<h1>COUCOU VINO</h1>
-
-<h2>ici on va imaginer qu'on est connecté en tant que user au id: 1</h2>
-<h3>quand tu vas créer un cellier ca va le lier au user_id 1 étant donné qu'on a pas fait la gestion du user encore, bisou</h3>
-<br>
-
-<a href="{{ route('cellier.index') }}"><button>Mes celliers</button></a>
-
-
-</body>
-</html>
+<h1>API VINO</h1>
+<p>Visitez le site <a href="https://vino.lesdeuxsauces.com">vino.lesdeuxsauces.com</a></p>
 
 </body>
 </html>
