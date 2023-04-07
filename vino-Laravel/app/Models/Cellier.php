e4b61afe269ac5fc30087d7fd3ec01dd4b87adeb
TommyFavreau <tommyfavreau@gmail.com>
2023-04-07 13:08:31 -0400

    Ajout de commentaires

--- vino-Laravel/app/Models/Cellier.php
+++ vino-Laravel/app/Models/Cellier.php
@@ -8,16 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Cellier extends Model
 {
   use HasFactory;
+  // Les attributs que l'on peut remplir dans la table "celliers"
   protected $fillable = [
     'nom',
     'user_id'
   ];
 
+  // Relation : un cellier appartient à un utilisateur
   public function cellierHasUser()
   {
     return $this->belongsTo(User::class);
   }
 
+  // Relation : un cellier a plusieurs bouteilles
   public function cellierHasBouteille()
   {
       return $this->belongsToMany(Bouteille::class, 'celliers_has_bouteilles')->withPivot('quantite');
